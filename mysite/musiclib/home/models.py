f1aa4d93a3706abb25c3c93f8ea99928f3bb19b2
DTeltsovKNU <dteltsov@knu.ua>
2021-03-09 21:04:55 +0200

    Commit

--- mysite/musiclib/home/models.py
+++ mysite/musiclib/home/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Albumsongs(models.Model):
-    id = models.IntegerField(primary_key=True)
-    songid = models.ForeignKey('Albums', models.DO_NOTHING, db_column='SongId')  # Field name made lowercase.
+    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='SongId')  # Field name made lowercase.
     albumid = models.IntegerField(db_column='AlbumId')  # Field name made lowercase.
 
     class Meta:
@@ -11,10 +10,9 @@ class Albumsongs(models.Model):
 
 
 class Albums(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField(db_column='Name')  # Field name made lowercase. This field type is a guess.
-    amount_of_songs = models.IntegerField(db_column='Amount_of_songs')  # Field name made lowercase.
-    release = models.DateField(db_column='Release')  # Field name made lowercase.
+    title = models.CharField(db_column='Title', max_length=50)  # Field name made lowercase.
+    amount_of_songs = models.IntegerField(db_column='Amount_of_songs', blank=True, null=True)  # Field name made lowercase.
+    release = models.DateField(db_column='Release', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -22,8 +20,7 @@ class Albums(models.Model):
 
 
 class Creator(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField(db_column='Name')  # Field name made lowercase. This field type is a guess.
+    name = models.CharField(db_column='Name', max_length=50)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -31,7 +28,6 @@ class Creator(models.Model):
 
 
 class Creatorsongs(models.Model):
-    id = models.IntegerField(primary_key=True)
     songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='SongId')  # Field name made lowercase.
     creatorid = models.ForeignKey(Creator, models.DO_NOTHING, db_column='CreatorId')  # Field name made lowercase.
 
@@ -41,9 +37,7 @@ class Creatorsongs(models.Model):
 
 
 class Genre(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField(db_column='Name')  # Field name made lowercase. This field type is a guess.
-    songid = models.IntegerField(db_column='SongId')  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=50)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -51,7 +45,6 @@ class Genre(models.Model):
 
 
 class Songgeners(models.Model):
-    id = models.IntegerField(primary_key=True)
     songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='SongId')  # Field name made lowercase.
     genreid = models.ForeignKey(Genre, models.DO_NOTHING, db_column='GenreId')  # Field name made lowercase.
 
@@ -61,18 +54,27 @@ class Songgeners(models.Model):
 
 
 class Songs(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField(db_column='Name')  # Field name made lowercase. This field type is a guess.
-    release = models.DateField(db_column='Release')  # Field name made lowercase.
-    text = models.TextField(db_column='Text')  # Field name made lowercase. This field type is a guess.
+    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
+    title = models.CharField(db_column='Title', max_length=50)  # Field name made lowercase.
+    release = models.DateField(db_column='Release', blank=True, null=True)  # Field name made lowercase.
+    text = models.TextField(db_column='Text', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    cover = models.TextField(db_column='Cover')  # Field name made lowercase.
+    creator = models.CharField(db_column='Creator', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    album = models.CharField(db_column='Album', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    genre = models.CharField(db_column='Genre', max_length=50, blank=True, null=True)  # Field name made lowercase.
 
+    def __str__(self):
+        return self.title
+            
+    
     class Meta:
+        verbose_name='Песни'
         managed = False
         db_table = 'Songs'
+        
 
 
 class Songsrelationships(models.Model):
-    id = models.IntegerField(primary_key=True)
     song1 = models.IntegerField(db_column='Song1')  # Field name made lowercase.
     song2 = models.ForeignKey(Songs, models.DO_NOTHING, db_column='Song2')  # Field name made lowercase.
     status = models.TextField(db_column='Status')  # Field name made lowercase. This field type is a guess.
@@ -82,9 +84,7 @@ class Songsrelationships(models.Model):
         db_table = 'SongsRelationships'
 
 
-
 class Usersongs(models.Model):
-    id = models.IntegerField(primary_key=True)
     songid = models.ForeignKey(Songs, models.DO_NOTHING, db_column='SongId')  # Field name made lowercase.
     userid = models.ForeignKey('Users', models.DO_NOTHING, db_column='UserId')  # Field name made lowercase.
 
@@ -94,9 +94,8 @@ class Usersongs(models.Model):
 
 
 class Users(models.Model):
-    id = models.IntegerField(primary_key=True)
+    name = models.CharField(db_column='Name', max_length=50)  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'Users'
-
