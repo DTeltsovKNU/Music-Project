6c4f5bb2d20fab2c8bd5a14730ee73a9f1955a77
DTeltsovKNU <dteltsov@knu.ua>
2021-03-23 17:53:39 +0200

    added ability to import\export data to\from  xls\csv\json\etc.

--- mysite/musiclib/home/admin.py
+++ mysite/musiclib/home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
@@ -16,21 +17,25 @@ class AlbumInline(admin.TabularInline):
     model = Albumsongs
     extra = 1  
 
-class SongsAdmin(admin.ModelAdmin):
+class SongsAdmin(ImportExportModelAdmin):
     inlines = [
         GenreInline,
         AlbumInline,
         CreatorInline,
     ]
 
-class AlbumsAdmin(admin.ModelAdmin):
+class AlbumsAdmin(ImportExportModelAdmin):
     list_display = ('title', 'release')
 
+class ExportAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(Songsrelationships)
+
+
+admin.site.register(Songsrelationships, ExportAdmin)
 admin.site.register(Songs, SongsAdmin)
-admin.site.register(Creator)
+admin.site.register(Creator, ExportAdmin)
 admin.site.register(Albums, AlbumsAdmin)
-admin.site.register(Users)
-admin.site.register(Genre)
+admin.site.register(Users, ExportAdmin)
+admin.site.register(Genre, ExportAdmin)
 
