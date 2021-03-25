74cd79303c5c2ec2580d58a91b8ab981f6adce90
DTeltsovKNU <dteltsov@knu.ua>
2021-03-25 16:47:59 +0200

    Added function to get data for charts

--- mysite/musiclib/home/admin.py
+++ mysite/musiclib/home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import *
 from import_export.admin import ImportExportModelAdmin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
 
 # Register your models here.
 
@@ -9,23 +12,52 @@ class GenreInline(admin.TabularInline):
     model = Songsgenre
     extra = 1   
 
+
+
 class CreatorInline(admin.TabularInline):
     model = Creatorsongs
     extra = 1  
+
+
 
 class AlbumInline(admin.TabularInline):
     model = Albumsongs
     extra = 1  
 
+
+@admin.register(Songs)
 class SongsAdmin(ImportExportModelAdmin):
     inlines = [
         GenreInline,
         AlbumInline,
         CreatorInline,
     ]
+    search_fields = ['title']
+    list_display = ('title', 'get_img')
 
+    def get_img(self, obj):
+        return mark_safe(f'<img src = {obj.cover} width = "50" height = "50"')
+
+    get_img.short_description = 'Обложка'
+
+@admin.register(Albums)
 class AlbumsAdmin(ImportExportModelAdmin):
     list_display = ('title', 'release')
+    list_filter = ['amount_of_songs']
+    search_fields = ['title']
+
+    change_list_template = 'admin\home\Albums\change_list.html'
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        data = Albums.objects.all()
+        context = {
+            'data':data
+        }
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=context)
+
 
 class ExportAdmin(ImportExportModelAdmin):
     pass
@@ -33,9 +65,7 @@ class ExportAdmin(ImportExportModelAdmin):
 
 
 admin.site.register(Songsrelationships, ExportAdmin)
-admin.site.register(Songs, SongsAdmin)
 admin.site.register(Creator, ExportAdmin)
-admin.site.register(Albums, AlbumsAdmin)
 admin.site.register(Users, ExportAdmin)
 admin.site.register(Genre, ExportAdmin)
 
